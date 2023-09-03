372e5f6201ebf5572d6ee88896d8d659e1f0704c
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-09-03 18:07:48 -0300

    feat: Cadastro scene (WIP)

--- src/application/Principal.java
+++ src/application/Principal.java
@@ -2,9 +2,9 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Principal extends Application{
@@ -15,8 +15,8 @@ public class Principal extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		AnchorPane anchorPane = FXMLLoader.load(this.getClass().getResource("Principal.FXML"));
-		Scene scene = new Scene(anchorPane);
+		Parent root = FXMLLoader.load(this.getClass().getResource("Principal.FXML"));
+		Scene scene = new Scene(root);
 		
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Perfumex");
--- src/application/controller/PrincipalController.java
+++ src/application/controller/PrincipalController.java
@@ -1,33 +1,39 @@
 package application.controller;
-
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import javafx.scene.control.Button;
-
-import javafx.scene.control.TextField;
-
-import javafx.scene.control.Label;
-
-import javafx.scene.image.ImageView;
+import java.io.IOException;
+import javafx.event.ActionEvent;
 
 public class PrincipalController {
-	@FXML
-	private ImageView imageViewLogin;
-	@FXML
-	private Label labelLogin;
-	@FXML
-	private Label labelEmail;
-	@FXML
-	private Label labelSenha;
-	@FXML
-	private TextField tFieldEmail;
-	@FXML
-	private TextField tFieldSenha;
-	@FXML
-	private Label labelNovaConta;
-	@FXML
-	private Button btnCadastrar;
-	@FXML
-	private Button btnEntrar;
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 
+	@FXML
+	public void cadastrar(ActionEvent event) {
+		System.out.println("Clicou!!");
+	}
+	
+	public void irParaLogin(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("/application/Principal.FXML"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	@FXML
+	public void irParaCadastro(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/application/PrincipalCadastro.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 }
