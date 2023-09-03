ae0b2b7c15878fe9eeec0b6c11b5fc94ad394908
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-09-03 15:55:38 -0300

    feat: Add icon

--- src/application/Principal.java
+++ src/application/Principal.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -19,6 +20,8 @@ public class Principal extends Application{
 		
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Perfumex");
+		Image icon = new Image("/assets/Icon.png");
+		primaryStage.getIcons().add(icon);
 		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
