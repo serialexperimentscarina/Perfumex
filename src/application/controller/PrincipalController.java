374d0d3ba37ec8786f1a55a2de3151782641e3b4
ByancaMatos01 <byancathmatos@gmail.com>
2023-11-03 17:15:28 -0300

    lojafeita

--- src/application/controller/PrincipalController.java
+++ src/application/controller/PrincipalController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -36,4 +37,16 @@ public class PrincipalController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+   
+	
+	
+	
+	public void irParaCadastrarLojista(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/application/Lojista.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+}
 }
