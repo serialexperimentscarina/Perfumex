d6f48efd66d4aeb5f7b7cf6053b3667dc29067f3
ByancaMatos01 <byancathmatos@gmail.com>
2023-11-03 19:17:25 -0300

    Parte CadastroClienteFeito

--- src/application/controller/PrincipalController.java
+++ src/application/controller/PrincipalController.java
@@ -37,6 +37,14 @@ public class PrincipalController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	@FXML
+	public void irParaCliente(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/application/Cliente.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 	
    
 	@FXML
