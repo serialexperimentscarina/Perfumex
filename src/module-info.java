4306ed29822da32294b892b0550d374b6afb7a4a
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-11-05 16:11:50 -0300

    feat: Connection to DB (WIP)

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module Perfumex {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens application.controller to javafx.fxml;
